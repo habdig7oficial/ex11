dd2763835996461d0bbd6d92061caa827ef40960
habdig7oficial <65318468+habdig7oficial@users.noreply.github.com>
2021-08-19 17:56:06 -0300

    Add files via upload

---
+++ Validador de pontes/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Validador_de_pontes
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Ponte Pizza (150kg)",
+            "Ponte Bonita (40kg)",
+            "Ponte simpatica (100kg)"});
+            this.comboBox1.Location = new System.Drawing.Point(310, 137);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(321, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Selecione a Ponte";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Insira a Altura do veículo";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(208, 243);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(208, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(327, 45);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Atestador de veículos";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(321, 425);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(420, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "e veículo + carga";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(433, 243);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 23);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(433, 288);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(76, 23);
+            this.textBox4.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(-7, -5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(821, 74);
+            this.panel1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 511);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Validador de pontes/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Validador_de_pontes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double pa, aat, av, vb, ct,  pt;
+
+
+            if(comboBox1.SelectedIndex == 0)
+            {
+                pa = 150;
+
+                aat = 80;
+            }
+
+
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                pa = 40;
+
+                aat = 50;
+            }
+
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                pa = 100;
+
+                aat = 30;
+            }
+
+            else
+            {
+                MessageBox.Show("Selecione a ponte desejada", "ops!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                return;
+            }
+
+
+            if (Double.TryParse(textBox1.Text,out av) == false || Double.TryParse(textBox3.Text, out vb)==false || Double.TryParse(textBox4.Text, out ct)==false) {
+
+                MessageBox.Show("Insira todos os dados necessários", "ops!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                return;
+            }
+
+
+            if (av >= aat)
+            {
+                MessageBox.Show("Altura Insuficiente", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            else
+            {
+                MessageBox.Show("Altura Suportada", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+            if ((vb + ct) >= pa)
+            {
+                MessageBox.Show("pesso maior que o suportado", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                return;
+            }
+
+            else
+            {
+                MessageBox.Show("peso Suportado", "Resultado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+
+        }
+    }
+}
